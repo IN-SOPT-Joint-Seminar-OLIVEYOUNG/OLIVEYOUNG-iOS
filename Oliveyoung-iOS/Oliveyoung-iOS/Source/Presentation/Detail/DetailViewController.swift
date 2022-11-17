12d827ff5a8861e751cae73d680dafef13b5f4bb
Joon Baek <joonbaek12@gmail.com>
2022-11-18 01:27:05 +0900

    😺[CHORE] Stash 추가 및 충돌 해결 (#3)

--- Oliveyoung-iOS/Oliveyoung-iOS/Source/Presentation/Detail/DetailViewController.swift
+++ Oliveyoung-iOS/Oliveyoung-iOS/Source/Presentation/Detail/DetailViewController.swift
@@ -24,7 +24,10 @@ final class DetailViewController: UIViewController {
     
     //MARK: - Components
     
-    private let productContainerView = UIView()
+    private let productContainerView = UIView().then {
+        $0.backgroundColor = .red
+    }
+    
     private let deliveryContainerView = UIView()
     private let availableStoreContainerView = UIView()
     private let productDetailContainerView = UIView()
@@ -35,27 +38,94 @@ final class DetailViewController: UIViewController {
     private let searchButton = UIButton()
     private let cartButton = UIButton()
     private let brandButton = UIButton()
+    private let heartButton = UIButton()
+    private let purchaseButton = UIButton()
     
-    private let brandLabel = UILabel()
-    private let productLabel = UILabel()
-    private let priceLabel = UILabel()
-    private let originalPriceLabel = UILabel()
-    private let discountLabel = UILabel()
-    private let rateLabel = UILabel()
-    private let rateCountLabel = UILabel()
-    private let productTypeLabel = UILabel()
-    private let shippingLabel = UILabel()
-    private let shippingPriceLabel = UILabel()
-    private let freeShippingLabel = UILabel()
-    private let overnightShippingLabel = UILabel()
-    private let overnightPriceLabel = UILabel()
-    private let overnightTimeLabel = UILabel()
+    private let brandLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = 0x2F2F2F.color
+        $0.text = "에스트라"
+    }
     
-    private let availableStoreLabel = UILabel()
-    private let storeStatusLabel = UILabel()
-    private let stockLabel = UILabel()
+    private let productLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 22, weight: .medium)
+        $0.textColor = 0x2F2F2F.color
+        
+    }
+    
+    private let priceLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 22, weight: .medium)
+        $0.textColor = 0x2F2F2F.color
+    }
+    
+    private let originalPriceLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 12, weight: .medium)
+        $0.textColor = 0xEBEBEB.color    }
+    
+    private let discountLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 22, weight: .medium)
+        $0.textColor = 0xF87171.color
+    }
+    
+    private let rateLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 12, weight: .medium)
+        $0.textColor = 0x2F2F2F.color
+    }
+    
+//    private let rateCountLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let productTypeLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let shippingLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let shippingPriceLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let freeShippingLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let overnightShippingLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let overnightPriceLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let overnightTimeLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let minuteLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let hourLable = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let availableStoreLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let storeStatusLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
+//
+//    private let stockLabel = UILabel().then {
+//        $0.textColor = 0x
+//    }
     
     private let productImageView = UIImage()
+    private let searchImageView = UIImage()
     
     
     //MARK: - Variables
@@ -65,8 +135,9 @@ final class DetailViewController: UIViewController {
     //MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+//        buttonConfig()
         layout()
-        configDelegate()
+//        configDelegate()
     }
 }
 
@@ -75,24 +146,44 @@ extension DetailViewController {
     
     //MARK: - Layout Helpers
     private func layout() {
+        //        view.addSubviews(productContainerView, deliveryContainerView, availableStoreContainerView, productDetailContainerView, productRecommandContainerView, relatedProductContainerView)
         
+        view.addSubview(productContainerView)
+        productContainerView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(60)
+        }
     }
     
     //MARK: - General Helpers
-    private func configDelegate() {
-        
-    }
+//    private func buttonConfig(){
+//        backButton.setImage(UIImage(named: <#T##String#>), for: .normal)
+//        searchButton.setImage(UIImage(named: "searchIcon24X24"), for: .normal)
+//        cartButton.setImage(UIImage(named: "shoppingBag22X22"), for: .normal)
+//        brandButton.setImage(UIImage(named: "chevron12X12"), for: .normal)
+////        heartButton.setImage(UIImage(named: <#T##String#>), for: .normal)
+//        purchaseButton.setTitle("구매하기", for: .normal)
+//    }
+    
+//    private func labelConfig() {
+//        brandLabel.
+//    }
+    
+//    private func configDelegate() {
+//
+//    }
     
     //MARK: - Action Helpers
     
 }
 
-//MARK: - UICollectionViewDelegateFlowLayout
-extension DetailViewController: UICollectionViewDelegateFlowLayout {
-    
-}
 
-//MARK: - UICollectionViewDataSource
+////MARK: - UICollectionViewDelegateFlowLayout
+//extension DetailViewController: UICollectionViewDelegateFlowLayout {
+//
+//}
+//
+////MARK: - UICollectionViewDataSource
 //extension DetailViewController: UICollectionViewDataSource {
 //    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 //        <#code#>
@@ -102,5 +193,4 @@ extension DetailViewController: UICollectionViewDelegateFlowLayout {
 //        <#code#>
 //    }
 //
-//    
 //}
---
+++ Oliveyoung-iOS/Oliveyoung-iOS/Source/Presentation/Detail/TagCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TagCollectionViewCell.swift
+//  Oliveyoung-iOS
+//
+//  Created by Joon Baek on 2022/11/17.
+//
+
+import UIKit
+
+final class TagCollectionViewCell: UICollectionViewCell {
+    
+}
