a988d16ceb97c438eea3ac448f5517eea8ae644e
jeongkite <jeong6kite@gmail.com>
2022-11-21 19:22:25 +0900

    ➕[ADD] 컬러 익스텐션 (X)

---
+++ Oliveyoung-iOS/Oliveyoung-iOS/Source/Common/Extensions/UIColor+.swift
@@ -0,0 +1,68 @@
+//
+//  UIColor+.swift
+//  Oliveyoung-iOS
+//
+//  Created by 양정연 on 2022/11/21.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+
+  @nonobjc class var willaGrayGray500: UIColor {
+    return UIColor(white: 47.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaGrayGray550: UIColor {
+    return UIColor(white: 68.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaGrayGray400: UIColor {
+    return UIColor(white: 113.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaGrayGray350: UIColor {
+    return UIColor(white: 142.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaGrayGray300: UIColor {
+    return UIColor(white: 167.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaGrayGray250: UIColor {
+    return UIColor(white: 194.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaGrayGray200: UIColor {
+    return UIColor(white: 220.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaGrayGray150: UIColor {
+    return UIColor(white: 235.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaGrayGray100: UIColor {
+    return UIColor(white: 246.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaGrayGray000: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaRedRed500: UIColor {
+    return UIColor(red: 239.0 / 255.0, green: 68.0 / 255.0, blue: 68.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaRedRed400: UIColor {
+    return UIColor(red: 248.0 / 255.0, green: 113.0 / 255.0, blue: 113.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willaYellow: UIColor {
+    return UIColor(red: 1.0, green: 183.0 / 255.0, blue: 0.0, alpha: 1.0)
+  }
+
+  @nonobjc class var willa: UIColor {
+    return UIColor(red: 164.0 / 255.0, green: 210.0 / 255.0, blue: 50.0 / 255.0, alpha: 1.0)
+  }
+
+}
