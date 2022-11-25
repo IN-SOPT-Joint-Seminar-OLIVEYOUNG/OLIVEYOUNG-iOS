e7a56ebfc4c978851d1bde0f515f290fccff9639
jeongkite <jeong6kite@gmail.com>
2022-11-25 10:07:58 +0900

    ➕[ADD] URL 상수 (#15)

---
+++ Oliveyoung-iOS/Oliveyoung-iOS/Source/Service/NetworkAPI/URLConstant.swift
@@ -0,0 +1,19 @@
+//
+//  URLConstant.swift
+//  Oliveyoung-iOS
+//
+//  Created by 양정연 on 2022/11/25.
+//
+
+import Foundation
+
+struct URLConstant {
+    // MARK: - Base
+    static let baseURL = "http://13.125.97.174:3000"
+    
+    static let mainPick = "/main/pick"
+    static let mainBrand = "/main/brand"
+    static let searchMain = "/search/main"
+    static let search = "/search"
+    static let product = "/product"
+}
